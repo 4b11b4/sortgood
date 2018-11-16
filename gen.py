75c25250ecc164382c058d749ac144affb7b00e1
AB <4b11b4@gmail.com>
2018-11-15 16:30:41 -0800

    implement timeit

--- gen.py
+++ gen.py
@@ -1,7 +1,33 @@
 import random
 import time
+import timeit
+import functools
 
-NUM = 50
-n = [random.randint(-50, 100) for c in range(NUM)]
+def basic_sort(n):
+  n.sort()
 
-print n
+if __name__ == '__main__':
+  NUM = 50
+  n = [random.randint(-50, 100) for c in range(NUM)]
+  print(n)
+
+  """
+  beg = time.perf_counter()
+  n.sort()
+  end = time.perf_counter()
+  print('dif: {} sec'.format(end-beg))
+  print(n)
+  """
+
+  t = timeit.Timer(functools.partial(basic_sort, n))
+  print(t.timeit(5))
+  
+  '''
+  print(timeit.timeit(stmt="basic_sort(n)", globals=globals(), \
+                      number=10000))
+
+  print(timeit.timeit(stmt="basic_sort(n)", globals=globals(), \
+                      setup="", number=10000))
+  '''
+
+  print(n)
